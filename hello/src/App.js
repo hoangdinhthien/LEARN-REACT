4244a489b986bab1bdae713eb41595e82c310c9c
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2023-02-19 15:31:21 +0700

    update-definition.js-useParams

--- hello/src/App.js
+++ hello/src/App.js
@@ -15,6 +15,7 @@ function App () {
           <Route path='/employees' element={<Employees />} />
           <Route path='/dictionary' element={<Dictionary />} />
           <Route path='/definition' element={<Definition />} />
+          <Route path='/definition/:search' element={<Definition />} />
           <Route path='/customers' element={<Customers />} />
 
         </Routes>
--- hello/src/pages/Definition.js
+++ hello/src/pages/Definition.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useParams } from 'react-router-dom';
 
 export default function Definition () {
     const [word, setWord] = useState();
+    let { search } = useParams();
 
     useEffect( () => {
-        fetch( 'https://api.dictionaryapi.dev/api/v2/entries/en/helicopter' )
+        fetch( 'https://api.dictionaryapi.dev/api/v2/entries/en/' + search )
             .then( ( response ) => response.json() )
             .then( ( data ) => {
                 setWord( data[0].meanings );
