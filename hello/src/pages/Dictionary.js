41dfa4aed35037d556dd964807d644454cfb17a8
hoangdinhthien <111034450+hoangdinhthien@users.noreply.github.com>
2023-02-19 23:11:19 +0700

    update-dictionary-and-search

--- hello/src/pages/Dictionary.js
+++ hello/src/pages/Dictionary.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-
+import DefinitionSearch from '../components/DefinitionSearch';
 
 
 export default function Dictionary () {
-    const [word, setWord] = useState( '' );
-    const [word2, setWord2] = useState( '' );
-
-    useEffect( () => {
-        console.log( 'State Updated', word );
-    }, [word] );
-
-    useEffect( () => {
-        console.log( 'State Updated', word2 );
-    }, [word2] );
-
-    //no dependency array --> update for any state change
-    //empty dependency array --> execute once
-    //passing in data --> only execute when those state variable are changed 
-
     return (
-        <>
-            <input type='text' onChange={( e ) => {
-                setWord( e.target.value );
-            }} />
-            <h2>Let's get the definition for {word}</h2>
-
-            <input type='text' onChange={( e ) => {
-                setWord2( e.target.value );
-            }} />
-            <h2>Let's get the definition for {word2}</h2>
-        </>
+        <div className='flex justify-center'>
+            <DefinitionSearch />
+        </div>
     );
+
 }
